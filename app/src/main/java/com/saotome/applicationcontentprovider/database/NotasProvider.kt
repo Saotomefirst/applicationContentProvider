fcba362db8142f532e001aa8b9c8108c55615b80
Saotomefirst <jorge.appleseed@gmail.com>
2021-12-23 00:30:46 -0300

    Implementados todos os métods do NotasProvider.kt
    Implementados métodos delete, insert, update e query para a NotasProvider.kt
    
    O método getType não foi implementado porque não será usado.

--- app/src/main/java/com/saotome/applicationcontentprovider/database/NotasProvider.kt
+++ app/src/main/java/com/saotome/applicationcontentprovider/database/NotasProvider.kt
@@ -2,51 +2,108 @@ package com.saotome.applicationcontentprovider.database
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.Context
 import android.content.UriMatcher
 import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.media.UnsupportedSchemeException
 import android.net.Uri
+import android.provider.BaseColumns._ID
+import com.saotome.applicationcontentprovider.database.NotasDatabaseHelper.Companion.TABELA_NOTAS
 
 class NotasProvider : ContentProvider() {
 
     // responsável pela validação da URI do ContentProvider
     private lateinit var mUriMatcher: UriMatcher
+    private lateinit var dbHelper: NotasDatabaseHelper
 
     // Responsável pela inicialização do ContentProvider
     override fun onCreate(): Boolean {
         mUriMatcher = UriMatcher(UriMatcher.NO_MATCH)
         mUriMatcher.addURI(AUTHORITY, "notas", NOTAS)
         mUriMatcher.addURI(AUTHORITY, "notas/#", NOTAS_POR_ID)
+
+        if (context != null) {
+            dbHelper = NotasDatabaseHelper(context as Context)
+        }
+
         return true
     }
 
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
-        TODO("Implement this to handle requests to delete one or more rows")
+        // Exclusão no banco sempre é feita por ID
+        if (mUriMatcher.match(uri) == NOTAS_POR_ID) {
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val linhasAfetadas: Int = db.delete(TABELA_NOTAS, "$_ID = ?", arrayOf(uri.lastPathSegment))
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return linhasAfetadas
+        }
+        else {
+            throw UnsupportedSchemeException("URI inválida para exclusão")
+        }
+
     }
 
     // Valida uma URI (ex: imagem)
     override fun getType(uri: Uri): String? {
-        TODO(
-            "Implement this to handle requests for the MIME type of the data" +
-                    "at the given URI"
-        )
+        // Usdo para requisições de arquivos, mas aqui não será usado
+        throw UnsupportedSchemeException("URI não implementada")
     }
 
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        TODO("Implement this to handle requests to insert a new row.")
+        if (mUriMatcher.match(uri) == NOTAS) {
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val id = db.insert(TABELA_NOTAS, null, values)
+            val insertURI: Uri = Uri.withAppendedPath(BASE_URI, id.toString())
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return insertURI
+        }else {
+            throw UnsupportedSchemeException("URI inválida para inserção")
+        }
     }
 
     override fun query(
         uri: Uri, projection: Array<String>?, selection: String?,
         selectionArgs: Array<String>?, sortOrder: String?
     ): Cursor? {
-        TODO("Implement this to handle query requests from clients.")
+        return when {
+            // qualquer outra coisa que não seja o ID
+            mUriMatcher.match(uri) == NOTAS -> {
+                val db: SQLiteDatabase = dbHelper.writableDatabase
+                val cursor: Cursor = db.query(TABELA_NOTAS, projection, selection, selectionArgs, null, null, sortOrder)
+                cursor.setNotificationUri((context as Context).contentResolver, uri)
+                db.close()
+                cursor
+            }
+            mUriMatcher.match(uri) == NOTAS_POR_ID -> {
+                val db: SQLiteDatabase = dbHelper.writableDatabase
+                val cursor: Cursor = db.query(TABELA_NOTAS, projection, "$_ID = ?", arrayOf(uri.lastPathSegment), null, null, sortOrder)
+                cursor.setNotificationUri((context as Context).contentResolver, uri)
+                db.close()
+                cursor
+            }
+            else -> {
+                throw UnsupportedSchemeException("URI não implementada para busca")
+            }
+        }
     }
 
     override fun update(
         uri: Uri, values: ContentValues?, selection: String?,
         selectionArgs: Array<String>?
     ): Int {
-        TODO("Implement this to handle requests to update one or more rows.")
+        if (mUriMatcher.match(uri) == NOTAS_POR_ID) {
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val linhasAfetadas: Int = db.update(TABELA_NOTAS, values, "$_ID = ?", arrayOf(uri.lastPathSegment))
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return linhasAfetadas
+        }
+        else {
+            throw UnsupportedSchemeException("URI não implementada para atualização")
+        }
     }
 
     companion object {
