4351448fcfad8a43d2b4b906487e53358b9e368f
Saotomefirst <jorge.appleseed@gmail.com>
2021-12-25 13:42:52 -0300

    Adicionados Métodos de Suporte para exbição dos dados do Banco de Dados
    Implementado LoaderMananger - parcialmente - na MainActivity.kt para fazer a gestão em segundo plano das consultas ao banco de dados
    
    Implementado o ViewBinding para recuperação de parâmetros da activity_main.xml
    
    activity_main.xml alterada de constraintLayout para CoordinatorLayout, já com um RecyclerView e o botão de adicionar novos registros
    
    Adicionados os elementos gráficos dos botões de adição e exclusão - disponibilizados pelo professor em https://drive.google.com/drive/folders/1hjXcdqngMJM_juXZLKmUp0ex0ODS40oZ?usp=sharing

--- app/src/main/java/com/saotome/applicationcontentprovider/MainActivity.kt
+++ app/src/main/java/com/saotome/applicationcontentprovider/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.saotome.applicationcontentprovider
 
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.saotome.applicationcontentprovider.database.NotasDatabaseHelper.Companion.TITULO_NOTAS
+import com.saotome.applicationcontentprovider.database.NotasProvider.Companion.URI_NOTAS
+import com.saotome.applicationcontentprovider.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+/*
+O LoaderManager é quem faz a busca em segundo plano do cursor que alimenta o RecyclerView,
+evitando erros de threads dentro da aplicação
+ */
+class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> {
+
+    private lateinit var activityMainbinding: ActivityMainBinding
+    private lateinit var notasRecyclerview : RecyclerView
+    private lateinit var notasAdd : FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainbinding = ActivityMainBinding.inflate(layoutInflater)
+        val activityMainView = activityMainbinding.root
+        setContentView(activityMainView)
+
+        notasAdd = activityMainbinding.notasAdd
+        notasAdd.setOnClickListener {}
+
+        notasRecyclerview = activityMainbinding.notasRecycler
+
+    }
+
+    // Instancia o que será buscado
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> =
+        CursorLoader(this, URI_NOTAS, null, null, null, TITULO_NOTAS)
+
+    // Pega os dados recebidos e permite sua manipulação
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        if (data != null) {
+
+        }
+    }
+
+    // Mata a pesquisa em segundo plano
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
     }
 }
